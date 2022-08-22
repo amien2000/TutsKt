5dbb992b22f393dafeb7230f0a40d8db690c84dc
Razak <husinabdulrazak@gmail.com>
2022-08-22 11:58:27 +0800

    ADD USER INTERFACE

---
+++ app/src/main/java/my/com/tutskt/UserInterface.kt
@@ -0,0 +1,21 @@
+package my.com.tutskt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class UserInterface : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var counter = 0
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(){
+            counter += 1
+            textView.text = counter.toString()
+        }
+    }
+}
